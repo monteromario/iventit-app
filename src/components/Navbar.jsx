05619f1e7e89dd71b6ed4c1f3dc2110f7467181a
monteromario <59968975+monteromario@users.noreply.github.com>
2021-05-11 23:05:13 +0200

    Init

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,12 @@ function Navigation() {
         <Navbar className="navbar">
             <Navbar.Brand href="/">
             <div className="d-inline-block align-top mr-2 navLogo" />
-                <span className="c4 navText"> i v e n t </span><strong><span className="c3">i t </span></strong>
+                <span className="c4 navText"> i · v e n t </span>· <strong><span className="c3">i t </span></strong>
             </Navbar.Brand>
             <Navbar.Toggle />
             <Navbar.Collapse className="justify-content-end">
                 <Navbar.Text>
-                    <small><span className="c4"><i class="fas fa-user mr-2"></i></span><a href="/login">Mario Montero</a></small>
+                    <small><span className="c4"><i className="fas fa-user mr-2"></i></span><a href="/login">Mario Montero</a></small>
                 </Navbar.Text>
             </Navbar.Collapse>
         </Navbar>
